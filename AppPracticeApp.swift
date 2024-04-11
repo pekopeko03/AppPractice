39d74446ff46ae05d58c24680fff036d5f7b4e95
ehara03 <147780786+pekopeko03@users.noreply.github.com>
2024-04-11 12:31:06 +0900

    message

---
+++ AppPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppPracticeApp.swift
+//  AppPractice
+//
+//  Created by cmStudent on 2024/04/11.
+//
+
+import SwiftUI
+
+@main
+struct AppPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  AppPractice
+//
+//  Created by cmStudent on 2024/04/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
